2bb3e6379459bc083cf0a739a22eeb58d3c59173
Spitzy <spitzy.dev@gmail.com>
2020-11-03 23:13:25 -0500

    created strings for all lower upper number and string. Created if statments to see what people wnat in password

--- script.js
+++ script.js
@@ -9,12 +9,36 @@ function generatepassword() {
    }
    else{
      alert("You did not choose a correct password length!");
+     return;
    }
    alert(passwordLength);
 
    alert("Please choose in the following prompts what features you would like on your password");
    
+   var lowerCase = confirm("Do you want lower case letters in your password?");
+   var upperCase = confirm("Do you want uppercase letters in your password?");
+   var numbers = confirm("Do you want numbers in your password?");
+   var specialChars = confirm("Do you want special characters in your password?");
+
+   alert("Lower Case "+ lowerCase + "Upper Case "+ upperCase + "Numbers "+ numbers + "specialChars "+ specialChars);
   
+   if (lowerCase === false && upperCase === false && numbers === false && specialChars === false){
+     alert("You didn't choose any correct characters");
+     return;
+   }
+   var password = "";
+   var lowerCaseString = "abcdefghigklmnopqrstuvwxyz";
+   var upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   var numbersString = "1234567890";
+   var specialString= " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+   
+   if (lowerCase === true){
+     password += lowerCaseString;
+     alert(password);
+
+   }
+
+
 } //Function closed bracket
 
 
